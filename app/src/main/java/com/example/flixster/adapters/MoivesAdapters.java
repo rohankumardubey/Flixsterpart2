da97e2d0dda661c7677607df6d6da157da8d4de5
USER <tavent118@rams.wssu.edu>
2021-03-22 13:53:04 -0400

    fix all error

--- app/src/main/java/com/example/flixster/adapters/MoivesAdapters.java
+++ app/src/main/java/com/example/flixster/adapters/MoivesAdapters.java
@@ -3,22 +3,29 @@ package com.example.flixster.adapters;
 
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.bumptech.glide.Glide;
-//import com.codepath.rkpandey.flixster.R;
-//import  com.codepath.rkpandey.flixster.models.Movie;
+import com.example.flixster.DetailActivity;
 import com.example.flixster.R;
 import com.example.flixster.models.Movie;
 
+import org.parceler.Parcels;
+
 import java.util.List;
+
+//import com.codepath.rkpandey.flixster.R;
+//import  com.codepath.rkpandey.flixster.models.Movie;
 
 
 public class MoivesAdapters extends RecyclerView.Adapter<MoivesAdapters.ViewHolder> {
@@ -56,7 +63,7 @@ public class MoivesAdapters extends RecyclerView.Adapter<MoivesAdapters.ViewHold
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-
+        RelativeLayout container;
         TextView textViewTitle;
         TextView textViewOverview;
         ImageView imageViewPoster;
@@ -66,12 +73,35 @@ public class MoivesAdapters extends RecyclerView.Adapter<MoivesAdapters.ViewHold
             textViewTitle = itemView.findViewById(R.id.tvTitle);
             textViewOverview = itemView.findViewById(R.id.tvOverview);
             imageViewPoster = itemView.findViewById(R.id.IvPoster);
+            container = itemView.findViewById(R.id.container);
+
         }
 
-        public void bind(Movie movie) {
+        public void bind( final Movie movie) {
             textViewTitle.setText(movie.getTitle());
             textViewOverview.setText(movie.getOverview());
+            String imageUrl;
+            //if phone is in landscape
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                // then imageUrl = back drop image
+                imageUrl = movie.getBackdropPath();
+            } else {
+                // else imageUrl = poster image
+                imageUrl = movie.getPosterPath();
+            }
             Glide.with(context).load(movie.getPosterPath()).into( imageViewPoster);
+
+          // !. Register click listener on the whole row
+            container.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                 //2. Navigate to a new activity  on tap
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra( "movie", Parcels.wrap(movies));
+                    context.startActivity(i);
+             }
+         });
+
         }
     }
 }
